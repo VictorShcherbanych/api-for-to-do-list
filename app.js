50e27e5814610bce62d92c25bd839e2bdcdf80b8
VictorShcherbanych <vjacheslav.fly@gmail.com>
2023-01-27 15:42:20 +0200

    add microservice architecture for database

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const db = require('./db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-
+const database = require('./models/users')
 const app = express();
+
 const jsonParser = express.json();
-  
+
 app.use(express.static(__dirname + "/public"));
 
 const generateAccessToken = (id) => {
@@ -31,8 +32,7 @@ app.get("/api/tasks", async function(req, res){
         if (pageNumber){
             offset = Number(pageNumber)*elements-elements
         }
-        const tasks = await db.query("SELECT * FROM tasks WHERE user_id = $1 LIMIT $2 OFFSET $3", [decodeData.id, elements, offset]);
-        res.json(tasks.rows);
+        res.json(await database.getTasks(decodeData, elements, offset));
     } catch (e) {
         console.log(e)}
 });
@@ -51,9 +51,8 @@ app.post("/api/tasks", jsonParser, async function (req, res) {
         };
         const decodeData = jwt.verify(token, process.env.SECRET);
         const userId = decodeData.id;
-        const newTask = await db.query("INSERT INTO tasks(name, descriptions, deadlines, prioritys, user_id) values($1, $2, $3, $4, $5) RETURNING *", [taskName, taskDescription, taskDeadline, taskPriority, userId]);
-        // const id = Math.max.apply(Math,users.map(function(o){return o.id;}))
-        res.json(newTask);
+                // const id = Math.max.apply(Math,users.map(function(o){return o.id;}))
+        res.json(await database.postTask(taskName, taskDescription, taskDeadline, taskPriority, userId));
     } catch (e) {
         console.log(e)
 }
@@ -66,14 +65,12 @@ app.post("/api/register", jsonParser, async function (req, res) {
         const userLastname = req.body.lastname;
         const userLogin = req.body.login;
         const userPassword = req.body.password;
-        const candidate = await db.query("SELECT * FROM users WHERE login = $1", [userLogin]);
+        const candidate = await database.getUser(userLogin);
         if (!candidate.rowCount == 0){
             return res.status(400).json({message: "Користувач уже зареєстрований"})
         }
         const hashPassword = await bcrypt.hash(userPassword, 7);
-
-        const newUser = await db.query("INSERT INTO users(name, lastname, login, password) values($1, $2, $3, $4) RETURNING *", [userName, userLastname, userLogin, hashPassword]);
-
+        await database.createUser(userName, userLastname, userLogin, hashPassword)
         // const id = Math.max.apply(Math,users.map(function(o){return o.id;}))
         res.json({message: `Користувача ${userName} ${userLastname} створено`});
     } catch(e) {
@@ -86,7 +83,7 @@ app.post("/api/login", jsonParser, async function (req, res) {
         if(!req.body) return res.sendStatus(400);
         const login = req.body.login;
         const password = req.body.password;
-        const user = await db.query("SELECT * FROM users WHERE login = $1", [login])
+        const user = await database.getUser(login)
         if (!user.rowCount){
             return res.status(400).json({message: "Користувач не зареєстрований"})
         }
@@ -104,7 +101,6 @@ app.post("/api/login", jsonParser, async function (req, res) {
 app.patch('/api/tasks', jsonParser, async function (req, res){
 
     try{
-        console.log(req);
         if (!req.body) return res.sendStatus(400);
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
@@ -113,8 +109,7 @@ app.patch('/api/tasks', jsonParser, async function (req, res){
         const decodeData = jwt.verify(token, process.env.SECRET);
         const userId = decodeData.id;
         const taskId = req.body.id;
-        const changeTask = await db.query('UPDATE tasks SET done = $1 WHERE user_id = $2 AND id = $3 RETURNING *', [true, userId, taskId]);
-        res.json(changeTask);
+        res.json(await database.doneTask(userId, taskId));
     } catch (e) {
         console.log(e)
     }
@@ -131,8 +126,7 @@ app.put('/api/tasks/descriptions', jsonParser, async function (req,res){
         if (!req.params) return res.sendStatus(400);
         const taskId = req.body.id;
         const description = req.body.descriptions;
-        let changeDescription = await db.query('UPDATE tasks SET descriptions = $1 WHERE id = $2 AND user_id = $3 RETURNING *', [description, taskId, userId]);
-        res.json(changeDescription);
+        res.json(await database.updateDescription(description, taskId, userId));
     } catch (e) {
         console.log(e)
     }   
@@ -146,9 +140,8 @@ app.delete('/api/tasks', jsonParser, async function(req, res){
             return res.status(400).json({message: "Користувач не авторизований"})};
         const decodeData = jwt.verify(token, process.env.SECRET)
         const userId = decodeData.id
-        const task_id = req.body.id;
-        let task = await db.query('DELETE FROM tasks WHERE id = $1 AND user_id = $2  RETURNING *', [task_id, userId]);
-        res.json(task.rows[0]);
+        const taskId = req.body.id;
+        res.json(await database.deleteTask(taskId, userId));
     } catch (e) {
         console.log(e)
     }
--- models/users.js
+++ models/users.js
@@ -1,2 +1,63 @@
 const db = require('../db');
 
+module.exports = {
+    getTasks: async function getTasks (user_id, elements, offset) {
+        try{
+            const tasks = await db.query("SELECT * FROM tasks WHERE user_id = $1 LIMIT $2 OFFSET $3", [user_id.id, elements, offset])
+            return tasks.rows
+        } catch (e) {
+            console.log(e)
+        }
+    },
+    postTask: async function postTask (taskName, taskDescription, taskDeadline, taskPriority, userId) {
+        try {
+            const newTask = await db.query("INSERT INTO tasks(name, descriptions, deadlines, prioritys, user_id) values($1, $2, $3, $4, $5) RETURNING *", [taskName, taskDescription, taskDeadline, taskPriority, userId]);
+            return newTask.rows
+        }catch (e) {
+            console.log(e)
+        }
+    },
+
+    getUser: async function getUser (userLogin) {
+        try{
+            const candidate = await db.query("SELECT * FROM users WHERE login = $1", [userLogin]);
+            return candidate
+        } catch (e) {
+            console.log (e)
+        }
+    },
+
+    createUser: async function createUser (userName, userLastname, userLogin, hashPassword) {
+        try{
+            const newUser = await db.query("INSERT INTO users(name, lastname, login, password) values($1, $2, $3, $4) RETURNING *", [userName, userLastname, userLogin, hashPassword]);
+            return newUser.rows
+        } catch (e) {
+            console.log(e)
+        }
+    },
+    doneTask: async function doneTask (userId, taskId) {
+        try{
+            const done = await db.query('UPDATE tasks SET done = $1 WHERE user_id = $2 AND id = $3 RETURNING *', [true, userId, taskId])
+            return done.rows
+        } catch (e) {
+            console.log(e)
+        }
+    },
+    updateDescription: async function updateDescription (description, taskId, userId) {
+        try{
+            let updateDescription = await db.query('UPDATE tasks SET descriptions = $1 WHERE id = $2 AND user_id = $3 RETURNING *', [description, taskId, userId]);
+            return updateDescription.rows
+        } catch (e) {
+            console.log(e)
+        }
+    },
+    deleteTask: async function deleteTask (taskId, userId) {
+        try {
+            const deleteTask = await db.query('DELETE FROM tasks WHERE id = $1 AND user_id = $2  RETURNING *' , [taskId, userId]);
+            return deleteTask.rows[0]
+        } catch (e) {
+            console.log (e)
+        }
+    }
+
+};
